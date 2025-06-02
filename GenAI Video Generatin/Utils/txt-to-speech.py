179e7f589003db31b1a2a1f8e91a91b70c31f776
Sankalp Jumde <135730661+SankalpJumde@users.noreply.github.com>
2025-06-02 16:53:58 +0530

    Delete GenAI Video Generatin/Utils/txt-to-speech.py

--- GenAI Video Generatin/Utils/txt-to-speech.py
+++
@@ -1,8 +0,0 @@
-from gtts import gTTS
-import os
-
-def text_to_speech(text, filename="output.mp3"):
-    tts = gTTS(text)
-    filepath = f"assets/audio/{filename}"
-    tts.save(filepath)
-    return filepath
